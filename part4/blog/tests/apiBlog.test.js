7904d8bf4c67b9a64edc4e8a1e4051003f7968c5
Dan-Lucian <danugaina1403@gmail.com>
2022-02-28 11:40:21 +0200

    fix: ex 4.10 names

--- part4/blog/tests/apiBlog.test.js
+++ part4/blog/tests/apiBlog.test.js
@@ -36,7 +36,7 @@ test('blog should have an id property', async () => {
 });
 
 test.only('should add a blog to the db', async () => {
-  const noteToAdd = {
+  const blogToAdd = {
     author: 'Author 3',
     title: 'Title 3',
     url: 'url 3',
@@ -45,7 +45,7 @@ test.only('should add a blog to the db', async () => {
 
   await api
     .post('/api/blogs')
-    .send(noteToAdd)
+    .send(blogToAdd)
     .expect(201)
     .expect('Content-Type', /application\/json/);
 
@@ -53,16 +53,16 @@ test.only('should add a blog to the db', async () => {
   expect(blogsAtEnd).toHaveLength(blogsInitial.length + 1);
 
   const authorsBlogs = blogsAtEnd.map((blog) => blog.author);
-  expect(authorsBlogs).toContain(noteToAdd.author);
+  expect(authorsBlogs).toContain(blogToAdd.author);
 
   const titlesBlogs = blogsAtEnd.map((blog) => blog.title);
-  expect(titlesBlogs).toContain(noteToAdd.title);
+  expect(titlesBlogs).toContain(blogToAdd.title);
 
   const urlsBlogs = blogsAtEnd.map((blog) => blog.url);
-  expect(urlsBlogs).toContain(noteToAdd.url);
+  expect(urlsBlogs).toContain(blogToAdd.url);
 
   const upvotesBlogs = blogsAtEnd.map((blog) => blog.upvotes);
-  expect(upvotesBlogs).toContain(noteToAdd.upvotes);
+  expect(upvotesBlogs).toContain(blogToAdd.upvotes);
 });
 
 afterAll(() => {
